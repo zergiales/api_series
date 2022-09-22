b1fa5ad6c371ec59a22b6ad4ef256c54f8ce60d4
sergio sanchez lopez <zergiosanchezlopez@gmail.com>
2022-09-22 19:32:46 +0200

    metodo para mostrar el repo

--- src/Controller/SeriesController.php
+++ src/Controller/SeriesController.php
@@ -12,8 +12,17 @@ class SeriesController extends AbstractController
 
     public function index(): Response
     {
+        //creamos el repositorio que va a sacar todas las series
+
+        //cargar un repositorio personalizado
+        $serie_repo = $this->getDoctrine()
+            ->getRepository(Serie::class);
+        //consulta
+        $ver_series = $serie_repo->findAll();
+
         return $this->render('series/index.html.twig', [
             'controller_name' => 'SeriesController',
+            'series' => $ver_series 
         ]);
     }
 
@@ -23,9 +32,9 @@ class SeriesController extends AbstractController
 
         //creo el objeto y  le doy valores
         $serie = new Serie();
-        $serie->setNombre('Suits');
-        $serie->setCreador(' Gabriel Macht');
-        $serie->setAnio(2011);
+        $serie->setNombre('serie de ejemplo');
+        $serie->setCreador(' sergio');
+        $serie->setAnio(2022);
 
 
         // Guardar objetos en doctrine
@@ -37,20 +46,43 @@ class SeriesController extends AbstractController
         return new Response('la serie guardada tiene el id: '.$serie->getId());
     }
 
-    public function ver($id){
+    public function ver(int $id): Response
+    {
         //cargar un repositorio personalizado
-        $serie_repo = $this->getDoctrine()->getRepository(Serie::class);
-
+        $serie_repo = $this->getDoctrine()
+        ->getRepository(Serie::class)
         //consulta
-        $serie = $serie_repo->find($id);
+        ->find($id);
 
         //comprobar si el resultado es correcto
-        if (!$serie) {
-            $message = 'La serie no existe';
+        if (!$serie_repo) {
+            throw $this->createNotFoundException(
+                'No esta la serie con el id: ' . $id
+            );
         }else{
-            $message = 'La serie es: '.$serie->getNombre().' y su director es: '.$serie->getCreador().' y se hizo
-            en el año: '.$serie->getAnio();
+            $message = 'La serie es: '.$serie_repo->getNombre().' y su director es: '.$serie_repo->getCreador().' y se hizo
+            en el año: '.$serie_repo->getAnio();
         }
         return new Response($message);
+    }
+
+    public function update($id){
+        //cargar Doctrine
+
+        //cargar entityManager
+
+        //cargar repo Serie
+
+        //Find para conseguir objeto
+        
+        //comprobar si el objeto me llega
+
+        //asignarle los valores al objeto
+
+        //persistir en doctrine
+
+        //guardar enbd
+
+        //respuesta
     }
 }
